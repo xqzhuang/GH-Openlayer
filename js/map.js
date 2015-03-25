e241fc22f10147753b4a4ef229e113214d68c373
Xiaoqin Zhuang <darlingxiaoqin@gmail.com>
2015-03-25 01:10:44 +0100

    Features popup

--- js/map.js
+++ js/map.js
@@ -1,4 +1,3 @@
-
 //Mouse position 
 var mousePositionControl = new ol.control.MousePosition({
   coordinateFormat: ol.coordinate.createStringXY(4),
@@ -84,81 +83,107 @@ var layerSwitcher = new ol.control.LayerSwitcher({
 map.addControl(layerSwitcher);
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+var styles = [
+  //Style for Polygon
+  new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#4682B4',
+      width: 2
+    }),
+    fill: new ol.style.Fill({
+      color: 'rgba(0, 0, 255, 0.1)'
+    })
+  }),
+  
+  //Style for marker
+  new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 5,
+      fill: new ol.style.Fill({
+        color: '#4682B4'
+      })
+    })
+  })
+];
 
-// assume features.json is a FeatureCollection in EPSG:4326
+// Parse features from file
 var vector = new ol.layer.Vector({
   source: new ol.source.GeoJSON({
     projection : 'EPSG:900913',
     url: 'js/geoJson.json'
-  })
+  }),
+  style: styles
 });
 
 map.addLayer(vector);
 
 //////////////////////////////////////////////////////////////////////////////
 //Popup
-
-// Get the popup div
-var element = document.getElementById('popup');
-
-// Instance the OL popup overlay
-var popup = new ol.Overlay({
-  element: element,
-  positioning: 'bottom-center',
-  stopEvent: false
-});
+//Use this great popup API
+var popup = new ol.Overlay.Popup();
 map.addOverlay(popup);
 
- var renderContent = function(feature){
-	  var entry = feature.get('entry');
-	  var view = {
-		//web : entry.gsx$web.$t,
-		//evento: entry.gsx$nombredelevento.$t
-	  };
-
-	  var template = "<a href={{web}}>{{evento}}</a>"
-
-	  return Mustache.render(template, view);
-};
-
-
-// display popup on click
+// Display popup on click
 map.on('click', function(evt) {
   // get the closest feature to the event point
   var feature = map.forEachFeatureAtPixel(evt.pixel,
 	  function(feature, layer) {
 		return feature;
 	  });
+
 	  
   // show the popup
   if (feature) {
 	var geometry = feature.getGeometry();
-	var coord = geometry.getCoordinates();
-	popup.setPosition(coord);
-	$(element).popover('destroy');
-	$(element).popover({
-	  'placement': 'top',
-	  'html': true,
-	  'content': 'I am a node! yeeeaaahhhhhh'
-	});
-	$(element).popover('show');
-  } else {
-	$(element).popover('destroy');
-  }
+	
+	if(geometry.getType() == 'Point') { //only popup when we click markers
+			 
+		var coord = geometry.getCoordinates();
+		var prettyCoord = ol.coordinate.toStringHDMS(ol.proj.transform(geometry.getCoordinates(), 'EPSG:3857', 'EPSG:4326'), 2);
+		
+		popup.show(coord, '<div><h2>Coordinates</h2><p>' + prettyCoord + '</p></div>');		//TODO: here to put the information of the node in the popup
+	} else if(geometry.getType() == 'Polygon') {
+		
+		//TODO: After clicking Polygon, it will show a popup which links to the specific room
+		var coord = geometry.getFirstCoordinate();		
+		popup.show(coord, '<a href="#">This is a polygon</a>');		//TODO: here to put the information of the node in the popup	
+	}	
+  } 
 });
 
 // change mouse cursor when over marker
-$(map.getViewport()).on('mousemove', function(e) {
-  var pixel = map.getEventPixel(e.originalEvent);
-  var hit = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-	return true;
+$(map.getViewport()).on('mousemove', function(evt) {
+  var pixel = map.getEventPixel(evt.originalEvent);
+  var feature = map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+	return feature;
   });
-  if (hit) {
+
+  if (feature) {
 	document.getElementById('map').style.cursor = 'pointer';
+	
   } else {
 	document.getElementById('map').style.cursor = '';
-  }
+  }     
 });
+
+///////////////////////////////////////////////////////////////////
+//trying to use interaction, seems easy
+// Interaction working on "pointermove"
+var selectPointerMove = new ol.interaction.Select({
+  condition: ol.events.condition.pointerMove
+});
+
+var changeInteraction = function() {
+  if (selectPointerMove !== null) {
+    map.removeInteraction(selectPointerMove);
+
+  }
+  
+  	map.addInteraction(selectPointerMove);
+};
+
+//Fire interaction
+changeInteraction();
 
 
 	
